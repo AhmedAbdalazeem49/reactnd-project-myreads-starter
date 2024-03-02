ed4f94bc4e3456474fa8ca19828a4207467cc734
AhmedAbdalazeem49 <99069962+AhmedAbdalazeem49@users.noreply.github.com>
2024-03-02 22:15:14 +0200

    work with API
    i have removed the static data for books
    then start working with the BOOKAPI given by Udacity
    to get all books and update their category.

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,7 +9,7 @@ const Book = ({ book , changeBookShelf}) => {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: `url(${book.url})`,
+            backgroundImage: `url(${book.imageLinks.thumbnail})`,
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -28,7 +28,7 @@ const Book = ({ book , changeBookShelf}) => {
         </div>
       </div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.author}</div>
+      <div className="book-authors">{book.authors[0]}</div>
     </div>
   );
 };
